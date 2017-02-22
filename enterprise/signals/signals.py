7a42c23ab199b5eebab1ab62b908dfea0b18454b
Justin Ellis <justin.ellis18@gmail.com>
2017-02-22 11:17:13 -0800

    More additions to signals.py

--- enterprise/signals/signals.py
+++ enterprise/signals/signals.py
@@ -15,9 +15,9 @@ class StochasticSignal(object):
     def __init__(self):
 
         self._param_dict = {}
-        self.signal_type = None
+        self.signal_type = 'stochastic'
 
-    def setup(self, psr):
+    def setup(self, psr, *args, **kwargs):
         """Abstract class for setting up signal model auxiliaries.
 
         Must be created by sub-classes.
@@ -32,6 +32,34 @@ class StochasticSignal(object):
         :param param: Instance of `Parameter` class.
         """
         self._param_dict[param.name] = param
+
+    def update_basis_functions(self, params, *args, **kwargs):
+        """Update basis functions with new parameters if necessary.
+
+        Must be created by sub-classes.
+
+        :param params: Parameter vector.
+
+        .. note: May want to use a different way of inputting parameters.
+        """
+        pass
+
+    def get_kernel_inv_det(self, params, *args, **kwargs):
+        """Compute the inverse of the Gaussian kernel.
+
+        This method computes the log-determinant and cholesky factor
+        of the rank reduced component of the gaussian kernel matrix
+        :math: `K = TBT^T`, where :math: `B` is the rank-reduced kernel
+        matrix. We compute :math: `logdet(B)` and :math: `B^{-1}`.
+
+        This method must be implemented by sub-classes
+
+        :param params: Parameter vector.
+
+        .. note: May want to use a different way of inputting parameters.
+
+        """
+        pass
 
     @property
     def param_vector(self):
