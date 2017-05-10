cd9b8f99f80aef273b84b37172a238d9f7a7ce30
Justin Ellis <justin.ellis18@gmail.com>
2017-05-09 21:38:36 -0700

    More tests, added Michele's signal collection

--- tests/test_gp_signals.py
+++ tests/test_gp_signals.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-test_signals
+test_gp_signals
 ----------------------------------
 
 Tests for signal modules.
@@ -50,11 +50,59 @@ class TestGPSignals(unittest.TestCase):
         # Jvec test
         jvec = 10**(2*ecorr) * np.ones(U.shape[1])
         msg = 'Prior vector incorrect for Basis Ecorr signal.'
-        assert np.all(ecm.get_phi(params)==jvec), msg
+        assert np.all(ecm.get_phi(params) == jvec), msg
 
         # inverse Jvec test
         msg = 'Prior vector inverse incorrect for Basis Ecorr signal.'
-        assert np.all(ecm.get_phiinv(params)==1/jvec), msg
+        assert np.all(ecm.get_phiinv(params) == 1/jvec), msg
+
+        # test shape
+        msg = 'U matrix shape incorrect'
+        assert ecm.basis_shape == U.shape, msg
+
+    def test_ecorr_backend(self):
+        """Test that ecorr-backend signal returns correct values."""
+        # set up signal parameter
+        ecorr = parameter.Uniform(-10, -5)
+        selection = Selection(selections.by_backend)
+        ec = gs.EcorrBasisModel(log10_ecorr=ecorr, selection=selection)
+        ecm = ec(self.psr)
+
+        # parameters
+        ecorrs = [-6.1, -6.2, -6.3, -6.4]
+        params = {'B1855+09_log10_ecorr_430_ASP': ecorrs[0],
+                  'B1855+09_log10_ecorr_430_PUPPI': ecorrs[1],
+                  'B1855+09_log10_ecorr_L-wide_ASP': ecorrs[2],
+                  'B1855+09_log10_ecorr_L-wide_PUPPI': ecorrs[3]}
+
+        # get the basis
+        bflags = self.psr.backend_flags
+        Umats = []
+        for flag in np.unique(bflags):
+            mask = bflags == flag
+            Umats.append(utils.create_quantization_matrix(self.psr.toas[mask]))
+        nepoch = sum(U.shape[1] for U in Umats)
+        U = np.zeros((len(self.psr.toas), nepoch))
+        jvec = np.zeros(nepoch)
+        netot = 0
+        for ct, flag in enumerate(np.unique(bflags)):
+            mask = bflags == flag
+            nn = Umats[ct].shape[1]
+            U[mask, netot:nn+netot] = Umats[ct]
+            jvec[netot:nn+netot] = 10**(2*ecorrs[ct])
+            netot += nn
+
+        # basis matrix test
+        msg = 'U matrix incorrect for Basis Ecorr-backend signal.'
+        assert np.allclose(U, ecm.get_basis(params)), msg
+
+        # Jvec test
+        msg = 'Prior vector incorrect for Basis Ecorr backend signal.'
+        assert np.all(ecm.get_phi(params) == jvec), msg
+
+        # inverse Jvec test
+        msg = 'Prior vector inverse incorrect for Basis Ecorr backend signal.'
+        assert np.all(ecm.get_phiinv(params) == 1/jvec), msg
 
         # test shape
         msg = 'U matrix shape incorrect'
@@ -82,15 +130,76 @@ class TestGPSignals(unittest.TestCase):
         # spectrum test
         phi = utils.powerlaw(f2, log10_A=log10_A, gamma=gamma) * f2[0]
         msg = 'Spectrum incorrect for GP Fourier signal.'
-        assert np.all(rnm.get_phi(params)==phi), msg
+        assert np.all(rnm.get_phi(params) == phi), msg
 
         # inverse spectrum test
         msg = 'Spectrum inverse incorrect for GP Fourier signal.'
-        assert np.all(rnm.get_phiinv(params)==1/phi), msg
+        assert np.all(rnm.get_phiinv(params) == 1/phi), msg
 
         # test shape
         msg = 'F matrix shape incorrect'
         assert rnm.basis_shape == F.shape, msg
+
+    def test_red_noise_add(self):
+        """Test that red noise addition only returns independent columns."""
+        # set up signals
+        pl = Function(utils.powerlaw, log10_A=parameter.Uniform(-18,-12),
+                      gamma=parameter.Uniform(1,7))
+        cpl = Function(utils.powerlaw,
+                       log10_A=parameter.Uniform(-18,-12)('log10_Agw'),
+                       gamma=parameter.Uniform(1,7)('gamma_gw'))
+
+        # parameters
+        log10_A, gamma = -14.5, 4.33
+        log10_Ac, gammac = -15.5, 1.33
+        params = {'B1855+09_log10_A': log10_A,
+                  'B1855+09_gamma': gamma,
+                  'log10_Agw': log10_Ac,
+                  'gamma_gw': gammac}
+
+        Tmax = self.psr.toas.max() - self.psr.toas.min()
+        tpars = [(30, 20, Tmax, Tmax), (20, 30, Tmax, Tmax),
+                 (30, 30, Tmax, Tmax), (30, 20, Tmax, 1.123*Tmax),
+                 (20, 30, Tmax, 1.123*Tmax), (30, 30, 1.123*Tmax, Tmax)]
+
+        for (nf1, nf2, T1, T2) in tpars:
+
+            rn = gs.FourierBasisGP(spectrum=pl, components=nf1, Tspan=T1)
+            crn = gs.FourierBasisGP(spectrum=cpl, components=nf2, Tspan=T2)
+            s = rn + crn
+            rnm = s(self.psr)
+
+            # set up frequencies
+            F1, f1, _ = utils.createfourierdesignmatrix_red(
+                self.psr.toas, nmodes=nf1, freq=True, Tspan=T1)
+            F2, f2, _ = utils.createfourierdesignmatrix_red(
+                self.psr.toas, nmodes=nf2, freq=True, Tspan=T2)
+
+            # test power spectrum
+            p1 = utils.powerlaw(f1, log10_A, gamma) * f1[0]
+            p2 = utils.powerlaw(f2, log10_Ac, gammac) * f2[0]
+            if T1 == T2:
+                nf = max(2*nf1, 2*nf2)
+                phi = np.zeros(nf)
+                F = F1 if nf1 > nf2 else F2
+                phi[:2*nf1] = p1
+                phi[:2*nf2] += p2
+                F[:,]
+            else:
+                phi = np.concatenate((p1, p2))
+                F = np.hstack((F1, F2))
+
+            msg = 'Combined red noise PSD incorrect '
+            msg += 'for {} {} {} {}'.format(nf1, nf2, T1, T2)
+            assert np.all(rnm.get_phi(params) == phi), msg
+
+            msg = 'Combined red noise PSD inverse incorrect '
+            msg += 'for {} {} {} {}'.format(nf1, nf2, T1, T2)
+            assert np.all(rnm.get_phiinv(params) == 1/phi), msg
+
+            msg = 'Combined red noise Fmat incorrect '
+            msg += 'for {} {} {} {}'.format(nf1, nf2, T1, T2)
+            assert np.allclose(F, rnm.get_basis(params)), msg
 
     def test_gp_timing_model(self):
         """Test that the timing model signal returns correct values."""
@@ -105,11 +214,11 @@ class TestGPSignals(unittest.TestCase):
         # Jvec test
         phi = np.ones(self.psr.Mmat.shape[1]) * 1e40
         msg = 'Prior vector incorrect for Timing Model signal.'
-        assert np.all(tm.get_phi()==phi), msg
+        assert np.all(tm.get_phi() == phi), msg
 
         # inverse Jvec test
         msg = 'Prior vector inverse incorrect for Timing Model signal.'
-        assert np.all(tm.get_phiinv()==1/phi), msg
+        assert np.all(tm.get_phiinv() == 1/phi), msg
 
         # test shape
         msg = 'M matrix shape incorrect'
