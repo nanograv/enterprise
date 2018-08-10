Fix delay caching, include GP prior computation in likelihood only optionally

enterprise/signals/gp_signals.py
@@ -95,7 +95,7 @@ def BasisGP(priorFunction, basisFunction, coefficients=None,
         # definitions) if the user wants it to model GP coefficients
         # (e.g., for a hierarchical likelihood) or if they do not
         if coefficients:
-            # MV: should I cache this?
+            @signal_base.cache_call('delay_params')
             def get_logprior(self, params):
                 self._construct_basis(params)
 
@@ -108,12 +108,18 @@ def BasisGP(priorFunction, basisFunction, coefficients=None,
                     c = params[par.name] if par.name in params else par.value
 
                     ret = (ret - 0.5 * np.sum(c * c / phi)
-                               - 0.5 * np.sum(np.log(phi))
-                               - 0.5 * len(phi) * math.log(2*math.pi))
+                               - 0.5 * np.sum(np.log(phi)))
+                               # - 0.5 * len(phi) * math.log(2*math.pi))
+                               # not included in signal_base likelihood
 
                 return ret
 
-            # MV: should I cache this?
+            # MV: could assign this to a data member at initialization
+            @property
+            def delay_params(self):
+                return [pp.name for pp in self.params if '_coefficients' in pp.name]
+
+            @signal_base.cache_call(['basis_params','delay_params'])
             def get_delay(self, params={}):
                 self._construct_basis(params)
 
@@ -135,6 +141,10 @@ def BasisGP(priorFunction, basisFunction, coefficients=None,
         else:
             def get_logprior(self, params):
                 return 0
+
+            @property
+            def delay_params(self):
+                return []
 
             def get_delay(self, params={}):
                 return 0            
@@ -161,7 +171,7 @@ def BasisGP(priorFunction, basisFunction, coefficients=None,
 
 def FourierBasisGP(spectrum, coefficients=None, components=20,
                    selection=Selection(selections.no_selection),
-                   Tspan=None, name=''):
+                   Tspan=None, name='red_noise'):
     """Convenience function to return a BasisGP class with a
     fourier basis."""
 
@@ -171,7 +181,7 @@ def FourierBasisGP(spectrum, coefficients=None, components=20,
     class FourierBasisGP(BaseClass):
         signal_type = 'basis'
         signal_name = 'red noise'
-        signal_id = name if name else 'red_noise'
+        signal_id = name
 
     return FourierBasisGP
 
@@ -208,7 +218,7 @@ def ecorr_basis_prior(weights, log10_ecorr=-8):
 def EcorrBasisModel(log10_ecorr=parameter.Uniform(-10, -5),
                     coefficients=None,
                     selection=Selection(selections.no_selection),
-                    name=''):
+                    name='basis_ecorr'):
     """Convenience function to return a BasisGP class with a
     quantized ECORR basis."""
 
@@ -219,7 +229,7 @@ def EcorrBasisModel(log10_ecorr=parameter.Uniform(-10, -5),
     class EcorrBasisModel(BaseClass):
         signal_type = 'basis'
         signal_name = 'basis ecorr'
-        signal_id = 'basis_ecorr_' + name if name else 'basis_ecorr'
+        signal_id = name
 
     return EcorrBasisModel
 

enterprise/signals/signal_base.py
@@ -163,9 +163,18 @@ class CommonSignal(Signal):
         return None
 
 
-class MarginalizedLogLikelihood(object):
-    def __init__(self, pta):
+def MarginalizedLogLikelihood(pta):
+    return LogLikelihood(pta)
+
+
+def HierarchicalLogLikelihood(pta):
+    return LogLikelihood(pta, detprior=True)
+
+
+class LogLikelihood(object):
+    def __init__(self, pta, detprior=False):
         self.pta = pta
+        self.detprior = detprior
 
     def _make_sigma(self, TNTs, phiinv):
         return sps.block_diag(TNTs,'csc') + sps.csc_matrix(phiinv)
@@ -175,7 +184,7 @@ class MarginalizedLogLikelihood(object):
         params = xs if isinstance(xs,dict) else self.pta.map_params(xs)
 
         # priors associated with delays evaluated as part of the likelihood
-        loglike = self.pta.get_logprior(params)
+        loglike = np.sum(self.pta.get_logprior(params)) if self.detprior else 0
 
         # phiinvs will be a list or may be a big matrix if spatially
         # correlated signals
@@ -712,8 +721,11 @@ def SignalCollection(metasignals):
                 if signal.signal_type == 'white noise':
                     self.white_params.extend(signal.ndiag_params)
                 elif signal.signal_type in ['basis', 'common basis']:
+                    # to support GP coefficients, and yet do the right thing
+                    # for common GPs, which do not have coefficients yet
+                    self.delay_params.extend(getattr(signal,'delay_params',[]))
                     self.basis_params.extend(signal.basis_params)
-                elif signal.signal_type == 'deterministic':
+                elif signal.signal_type in ['deterministic']:
                     self.delay_params.extend(signal.delay_params)
                 else:
                     msg = '{} signal type not recognized! Caching '.format(
@@ -799,8 +811,7 @@ def SignalCollection(metasignals):
             ndiags = [signal.get_ndiag(params) for signal in self._signals]
             return sum(ndiag for ndiag in ndiags if ndiag is not None)
 
-        # MV disabling this cache that does not seem to work at the moment
-        # @cache_call('delay_params')
+        @cache_call('delay_params')
         def get_logprior(self, params):
             return sum(signal.get_logprior(params) for signal in self._signals)
 