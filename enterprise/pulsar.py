b6808ade5996d188c3943c89c08c780609c13a37
Sarah Vigeland <s.vigeland@gmail.com>
2019-05-19 16:13:20 -0500

    used logger for warning messages in pulsar.py (#192)
    * used logger for warning messages
    
    * moved import logging statement to top block
    
    * changed location of statements to set up logging

--- enterprise/pulsar.py
+++ enterprise/pulsar.py
@@ -11,6 +11,7 @@ from ephem import Ecliptic, Equatorial
 import os
 import json
 from enterprise.signals import utils
+import logging
 
 try:
     import cPickle as pickle
@@ -38,6 +39,10 @@ import astropy.units as u
 if pint is None and t2 is None:
     err_msg = 'Must have either PINT or libstempo timing package installed'
     raise ImportError(err_msg)
+
+logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def get_maxobs(timfile):
@@ -79,11 +84,10 @@ class BasePulsar(object):
             pdist = tuple(pdict.get(self.name, (1.0, 0.2)))
 
         if pdist == (1.0, 0.2):
-            # TODO: should use logging here
             msg = 'WARNING: Could not find pulsar distance for '
             msg += 'PSR {0}.'.format(self.name)
             msg += ' Setting value to 1 with 20% uncertainty.'
-            print(msg)
+            logger.warning(msg)
         return pdist
 
     def _get_radec_from_ecliptic(self, elong, elat):
@@ -100,12 +104,11 @@ class BasePulsar(object):
             raj = np.double(eq.ra)
             decj = np.double(eq.dec)
 
-        # TODO: should use logging here
         except TypeError:
             msg = 'WARNING: Cannot fine sky location coordinates '
             msg += 'for PSR {0}. '.format(self.name)
             msg += 'Setting values to 0.0'
-            print(msg)
+            logger.warning(msg)
             raj = 0.0
             decj = 0.0
 
