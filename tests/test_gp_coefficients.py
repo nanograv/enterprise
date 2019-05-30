1a1c9d9955c176d0cfbb1c40e4875eecbd870e0b
Stephen Taylor <srtaylor@caltech.edu>
2019-05-30 16:02:43 -0700

    Editing utils.py to patch py27 functionality (#193)
    * adding ability to have user-defined modes in CommonGP
    
    * fixing lint issues
    
    * adding dynamic partial files
    
    * adding code for dynamic BayesEphem
    
    * fixing ephemeris tests
    
    * fixing ephemeris tests
    
    * fixing ephemeris tests
    
    * fixing to allow py27 support
    
    * running travis tests on py2 and py3
    
    * changing numpy requirements
    
    * changing python requirements
    
    * patching to avoid crashing when python2 is used with gp coefficients
    
    * final lint corrections

--- tests/test_gp_coefficients.py
+++ tests/test_gp_coefficients.py
@@ -12,6 +12,8 @@ Tests for GP signals used with deterministic coefficients.
 import math
 import unittest
 import numpy as np
+import platform
+import logging
 
 from tests.enterprise_test_data import datadir
 from enterprise.pulsar import Pulsar
@@ -23,6 +25,12 @@ from enterprise.signals import gp_signals
 from enterprise.signals import deterministic_signals
 from enterprise.signals import signal_base
 from enterprise.signals import utils
+
+pyv3 = platform.python_version().split('.')[0] == '3'
+
+logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @signal_base.function
@@ -52,226 +60,240 @@ class TestGPCoefficients(unittest.TestCase):
         cls.psr2 = Pulsar(datadir + '/B1937+21_NANOGrav_9yv1.gls.par',
                           datadir + '/B1937+21_NANOGrav_9yv1.tim')
 
-    def test_ephemeris(self):
-        """Test physical-ephemeris delay, made three ways: from marginalized
-        GP, from coefficient-based GP, from deterministic model."""
+    if not pyv3:
+        msg = 'GP coefficients compatible only with Python 3. Skipping tests'
+        logger.warning(msg)
+        pass
+    else:
+        def test_ephemeris(self):
+            """Test physical-ephemeris delay, made three ways: from
+            marginalized GP, from coefficient-based GP, from
+            deterministic model."""
 
-        ef = white_signals.MeasurementNoise(efac=parameter.Uniform(0.1, 5.0))
+            ef = white_signals.MeasurementNoise(
+                efac=parameter.Uniform(0.1, 5.0))
 
-        eph = gp_signals.FourierBasisCommonGP_physicalephem(
-            sat_orb_elements=None)
+            eph = gp_signals.FourierBasisCommonGP_physicalephem(
+                sat_orb_elements=None)
 
-        ephc = gp_signals.FourierBasisCommonGP_physicalephem(
-            sat_orb_elements=None, coefficients=True)
+            ephc = gp_signals.FourierBasisCommonGP_physicalephem(
+                sat_orb_elements=None, coefficients=True)
 
-        ephd = deterministic_signals.PhysicalEphemerisSignal(
-            inc_saturn_orb=False)
+            ephd = deterministic_signals.PhysicalEphemerisSignal(
+                inc_saturn_orb=False)
 
-        model = ef + eph
-        modelc = ef + ephc
-        modeld = ef + ephd
+            model = ef + eph
+            modelc = ef + ephc
+            modeld = ef + ephd
 
-        pta = signal_base.PTA([model(self.psr), model(self.psr2)])
-        ptac = signal_base.PTA([modelc(self.psr), modelc(self.psr2)])
-        ptad = signal_base.PTA([modeld(self.psr), modeld(self.psr2)])
+            pta = signal_base.PTA([model(self.psr), model(self.psr2)])
+            ptac = signal_base.PTA([modelc(self.psr), modelc(self.psr2)])
+            ptad = signal_base.PTA([modeld(self.psr), modeld(self.psr2)])
 
-        cf = 1e-3 * np.random.randn(11)
-        cf[0] = 1e-5  # this is more sensitive to linearity
+            cf = 1e-3 * np.random.randn(11)
+            cf[0] = 1e-5  # this is more sensitive to linearity
 
-        bs = pta.get_basis()
-        da = [np.dot(bs[0], cf), np.dot(bs[1], cf)]
+            bs = pta.get_basis()
+            da = [np.dot(bs[0], cf), np.dot(bs[1], cf)]
 
-        params = {'B1855+09_efac': 1, 'B1937+21_efac': 1,
-                  'B1855+09_physicalephem_gp_coefficients': cf,
-                  'B1937+21_physicalephem_gp_coefficients': cf}
-        db = ptac.get_delay(params=params)
+            params = {'B1855+09_efac': 1, 'B1937+21_efac': 1,
+                      'B1855+09_physicalephem_gp_coefficients': cf,
+                      'B1937+21_physicalephem_gp_coefficients': cf}
+            db = ptac.get_delay(params=params)
 
-        dparams = {'B1855+09_efac': 1, 'B1937+21_efac': 1,
-                   'frame_drift_rate': cf[0],
-                   'd_jupiter_mass': cf[1], 'd_saturn_mass': cf[2],
-                   'd_uranus_mass': cf[3], 'd_neptune_mass': cf[4],
-                   'jup_orb_elements': cf[5:]}
-        dc = ptad.get_delay(params=dparams)
+            dparams = {'B1855+09_efac': 1, 'B1937+21_efac': 1,
+                       'frame_drift_rate': cf[0],
+                       'd_jupiter_mass': cf[1], 'd_saturn_mass': cf[2],
+                       'd_uranus_mass': cf[3], 'd_neptune_mass': cf[4],
+                       'jup_orb_elements': cf[5:]}
+            dc = ptad.get_delay(params=dparams)
 
-        msg = "Reconstructed ephemeris signals differ!"
+            msg = "Reconstructed ephemeris signals differ!"
 
-        assert np.allclose(da[0], db[0]), msg
-        assert np.allclose(da[1], db[1]), msg
+            assert np.allclose(da[0], db[0]), msg
+            assert np.allclose(da[1], db[1]), msg
 
-        # we don't expect an exact match since we are linearizing
-        assert np.allclose(da[0], dc[0], atol=1e-3), msg
-        assert np.allclose(da[1], dc[1], atol=1e-3), msg
+            # we don't expect an exact match since we are linearizing
+            assert np.allclose(da[0], dc[0], atol=1e-3), msg
+            assert np.allclose(da[1], dc[1], atol=1e-3), msg
 
-    def test_common_red_noise(self):
-        """Test of a coefficient-based common GP."""
-        pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
-                            gamma=parameter.Uniform(1,7))
+        def test_common_red_noise(self):
+            """Test of a coefficient-based common GP."""
+            pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
+                                gamma=parameter.Uniform(1,7))
 
-        ef = white_signals.MeasurementNoise(efac=parameter.Uniform(0.1, 5.0))
+            ef = white_signals.MeasurementNoise(
+                efac=parameter.Uniform(0.1, 5.0))
 
-        Tspan = (max(self.psr.toas.max(), self.psr2.toas.max()) -
-                 min(self.psr.toas.max(), self.psr2.toas.max()))
+            Tspan = (max(self.psr.toas.max(), self.psr2.toas.max()) -
+                     min(self.psr.toas.max(), self.psr2.toas.max()))
 
-        pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
-                            gamma=parameter.Uniform(1,7))
+            pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
+                                gamma=parameter.Uniform(1,7))
 
-        rn = gp_signals.FourierBasisCommonGP(spectrum=pl, orf=utils.hd_orf(),
-                                             components=20, Tspan=Tspan)
+            rn = gp_signals.FourierBasisCommonGP(spectrum=pl,
+                                                 orf=utils.hd_orf(),
+                                                 components=20, Tspan=Tspan)
 
-        model = ef + rn
+            model = ef + rn
 
-        rnc = gp_signals.FourierBasisCommonGP(spectrum=pl, orf=utils.hd_orf(),
-                                              components=20, Tspan=Tspan,
-                                              coefficients=True)
+            rnc = gp_signals.FourierBasisCommonGP(spectrum=pl,
+                                                  orf=utils.hd_orf(),
+                                                  components=20, Tspan=Tspan,
+                                                  coefficients=True)
 
-        modelc = ef + rnc
+            modelc = ef + rnc
 
-        pta = signal_base.PTA([model(self.psr), model(self.psr2)])
-        ptac = signal_base.PTA([modelc(self.psr), modelc(self.psr2)])
+            pta = signal_base.PTA([model(self.psr), model(self.psr2)])
+            ptac = signal_base.PTA([modelc(self.psr), modelc(self.psr2)])
 
-        params = {'B1855+09_efac': 1.0,
-                  'B1937+21_efac': 1.0,
-                  'common_fourier_gamma': 5,
-                  'common_fourier_log10_A': -15}
+            params = {'B1855+09_efac': 1.0,
+                      'B1937+21_efac': 1.0,
+                      'common_fourier_gamma': 5,
+                      'common_fourier_log10_A': -15}
 
-        # get GP delays in two different ways
+            # get GP delays in two different ways
 
-        cf, cf2 = np.random.randn(40), np.random.randn(40)
+            cf, cf2 = np.random.randn(40), np.random.randn(40)
 
-        bs = pta.get_basis(params)
-        da = [np.dot(bs[0], cf), np.dot(bs[1], cf2)]
+            bs = pta.get_basis(params)
+            da = [np.dot(bs[0], cf), np.dot(bs[1], cf2)]
 
-        params.update({'B1855+09_common_fourier_coefficients': cf,
-                       'B1937+21_common_fourier_coefficients': cf2})
+            params.update({'B1855+09_common_fourier_coefficients': cf,
+                           'B1937+21_common_fourier_coefficients': cf2})
 
-        db = ptac.get_delay(params)
+            db = ptac.get_delay(params)
 
-        msg = 'Implicit and explicit GP delays are different.'
-        assert np.allclose(da[0], db[0]), msg
-        assert np.allclose(da[1], db[1]), msg
+            msg = 'Implicit and explicit GP delays are different.'
+            assert np.allclose(da[0], db[0]), msg
+            assert np.allclose(da[1], db[1]), msg
 
-        cpar = [p for p in ptac.params if 'coefficients' in p.name]
+            cpar = [p for p in ptac.params if 'coefficients' in p.name]
 
-        def shouldfail():
-            return cpar[0].get_logpdf(params)
+            def shouldfail():
+                return cpar[0].get_logpdf(params)
 
-        self.assertRaises(NotImplementedError, shouldfail)
+            self.assertRaises(NotImplementedError, shouldfail)
 
-    def test_fourier_red_noise(self):
-        """Test that implicit and explicit GP delays are the same."""
-        # set up signal parameter
-        pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
-                            gamma=parameter.Uniform(1,7))
-        rn = gp_signals.FourierBasisGP(spectrum=pl, components=20)
-        rnm = rn(self.psr)
+        def test_fourier_red_noise(self):
+            """Test that implicit and explicit GP delays are the same."""
+            # set up signal parameter
+            pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
+                                gamma=parameter.Uniform(1,7))
+            rn = gp_signals.FourierBasisGP(spectrum=pl, components=20)
+            rnm = rn(self.psr)
 
-        rnc = gp_signals.FourierBasisGP(spectrum=pl, components=20,
-                                        coefficients=True)
-        rnmc = rnc(self.psr)
+            rnc = gp_signals.FourierBasisGP(spectrum=pl, components=20,
+                                            coefficients=True)
+            rnmc = rnc(self.psr)
 
-        # parameters
-        log10_A, gamma = -14.5, 4.33
-        params = {'B1855+09_red_noise_log10_A': log10_A,
-                  'B1855+09_red_noise_gamma': gamma}
+            # parameters
+            log10_A, gamma = -14.5, 4.33
+            params = {'B1855+09_red_noise_log10_A': log10_A,
+                      'B1855+09_red_noise_gamma': gamma}
 
-        # get the GP delays in two different ways
-        cf = np.random.randn(40)
-        d1 = np.dot(rnm.get_basis(params),cf)
+            # get the GP delays in two different ways
+            cf = np.random.randn(40)
+            d1 = np.dot(rnm.get_basis(params),cf)
 
-        params.update({'B1855+09_red_noise_coefficients': cf})
-        d2 = rnmc.get_delay(params)
+            params.update({'B1855+09_red_noise_coefficients': cf})
+            d2 = rnmc.get_delay(params)
 
-        msg = 'Implicit and explicit GP delays are different.'
-        assert np.allclose(d1, d2), msg
+            msg = 'Implicit and explicit GP delays are different.'
+            assert np.allclose(d1, d2), msg
 
-        # np.array cast is needed because we get a KernelArray
-        phimat = np.array(rnm.get_phi(params))
-        pr1 = (-0.5 * np.sum(cf*cf/phimat) -
-               0.5 * np.sum(np.log(phimat)) -
-               0.5 * len(phimat) * np.log(2*math.pi))
+            # np.array cast is needed because we get a KernelArray
+            phimat = np.array(rnm.get_phi(params))
+            pr1 = (-0.5 * np.sum(cf*cf/phimat) -
+                   0.5 * np.sum(np.log(phimat)) -
+                   0.5 * len(phimat) * np.log(2*math.pi))
 
-        cpar = [p for p in rnmc.params if 'coefficients' in p.name][0]
-        pr2 = cpar.get_logpdf(params=params)
+            cpar = [p for p in rnmc.params if 'coefficients' in p.name][0]
+            pr2 = cpar.get_logpdf(params=params)
 
-        msg = 'Implicit and explicit GP priors are different.'
-        assert np.allclose(pr1, pr2), msg
+            msg = 'Implicit and explicit GP priors are different.'
+            assert np.allclose(pr1, pr2), msg
 
-    def test_ecorr_backend(self):
-        """Test that ecorr-backend signal returns correct values."""
-        # set up signal parameter
-        ecorr = parameter.Uniform(-10, -5)
-        selection = Selection(selections.by_backend)
-        ec = gp_signals.EcorrBasisModel(log10_ecorr=ecorr,
-                                        selection=selection)
-        ecm = ec(self.psr)
+        def test_ecorr_backend(self):
+            """Test that ecorr-backend signal returns correct values."""
+            # set up signal parameter
+            ecorr = parameter.Uniform(-10, -5)
+            selection = Selection(selections.by_backend)
+            ec = gp_signals.EcorrBasisModel(log10_ecorr=ecorr,
+                                            selection=selection)
+            ecm = ec(self.psr)
 
-        ecc = gp_signals.EcorrBasisModel(log10_ecorr=ecorr,
-                                         selection=selection,
-                                         coefficients=True)
-        eccm = ecc(self.psr)
+            ecc = gp_signals.EcorrBasisModel(log10_ecorr=ecorr,
+                                             selection=selection,
+                                             coefficients=True)
+            eccm = ecc(self.psr)
 
-        # parameters
-        ecorrs = [-6.1, -6.2, -6.3, -6.4]
-        params = {'B1855+09_basis_ecorr_430_ASP_log10_ecorr': ecorrs[0],
-                  'B1855+09_basis_ecorr_430_PUPPI_log10_ecorr': ecorrs[1],
-                  'B1855+09_basis_ecorr_L-wide_ASP_log10_ecorr': ecorrs[2],
-                  'B1855+09_basis_ecorr_L-wide_PUPPI_log10_ecorr': ecorrs[3]}
+            # parameters
+            ecorrs = [-6.1, -6.2, -6.3, -6.4]
+            params = {'B1855+09_basis_ecorr_430_ASP_log10_ecorr': ecorrs[0],
+                      'B1855+09_basis_ecorr_430_PUPPI_log10_ecorr': ecorrs[1],
+                      'B1855+09_basis_ecorr_L-wide_ASP_log10_ecorr': ecorrs[2],
+                      'B1855+09_basis_ecorr_L-wide_PUPPI_log10_ecorr':
+                          ecorrs[3]}
 
-        fmat = ecm.get_basis(params)
-        cf = 1e-6 * np.random.randn(fmat.shape[1])
-        d1 = np.dot(fmat, cf)
+            fmat = ecm.get_basis(params)
+            cf = 1e-6 * np.random.randn(fmat.shape[1])
+            d1 = np.dot(fmat, cf)
 
-        for key in ecm._keys:
-            parname = 'B1855+09_basis_ecorr_' + key + '_coefficients'
-            params[parname] = cf[ecm._slices[key]]
-        d2 = eccm.get_delay(params)
+            for key in ecm._keys:
+                parname = 'B1855+09_basis_ecorr_' + key + '_coefficients'
+                params[parname] = cf[ecm._slices[key]]
+            d2 = eccm.get_delay(params)
 
-        msg = 'Implicit and explicit ecorr-basis delays are different.'
-        assert np.allclose(d1, d2), msg
+            msg = 'Implicit and explicit ecorr-basis delays are different.'
+            assert np.allclose(d1, d2), msg
 
-    def test_formalism(self):
-        # create marginalized model
-        ef = white_signals.MeasurementNoise(efac=parameter.Uniform(0.1, 5.0))
-        tm = gp_signals.TimingModel()
-        ec = gp_signals.EcorrBasisModel(log10_ecorr=parameter.Uniform(-10, -5))
-        pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
-                            gamma=parameter.Uniform(1,7))
-        rn = gp_signals.FourierBasisGP(spectrum=pl, components=10)
-        model = ef + tm + ec + rn
-        pta = signal_base.PTA([model(self.psr)])
+        def test_formalism(self):
+            # create marginalized model
+            ef = white_signals.MeasurementNoise(
+                efac=parameter.Uniform(0.1, 5.0))
+            tm = gp_signals.TimingModel()
+            ec = gp_signals.EcorrBasisModel(
+                log10_ecorr=parameter.Uniform(-10, -5))
+            pl = utils.powerlaw(log10_A=parameter.Uniform(-18,-12),
+                                gamma=parameter.Uniform(1,7))
+            rn = gp_signals.FourierBasisGP(spectrum=pl, components=10)
+            model = ef + tm + ec + rn
+            pta = signal_base.PTA([model(self.psr)])
 
-        # create hierarchical model
-        tmc = gp_signals.TimingModel(coefficients=True)
-        ecc = gp_signals.EcorrBasisModel(log10_ecorr=parameter.Uniform(-10,-5),
-                                         coefficients=True)
-        rnc = gp_signals.FourierBasisGP(spectrum=pl, components=10,
-                                        coefficients=True)
-        modelc = ef + tmc + ecc + rnc
-        ptac = signal_base.PTA([modelc(self.psr)])
+            # create hierarchical model
+            tmc = gp_signals.TimingModel(coefficients=True)
+            ecc = gp_signals.EcorrBasisModel(
+                log10_ecorr=parameter.Uniform(-10,-5),
+                coefficients=True)
+            rnc = gp_signals.FourierBasisGP(spectrum=pl, components=10,
+                                            coefficients=True)
+            modelc = ef + tmc + ecc + rnc
+            ptac = signal_base.PTA([modelc(self.psr)])
 
-        ps = {'B1855+09_efac': 1,
-              'B1855+09_basis_ecorr_log10_ecorr': -6,
-              'B1855+09_red_noise_log10_A': -14,
-              'B1855+09_red_noise_gamma': 3}
-        psc = utils.get_coefficients(pta, ps)
+            ps = {'B1855+09_efac': 1,
+                  'B1855+09_basis_ecorr_log10_ecorr': -6,
+                  'B1855+09_red_noise_log10_A': -14,
+                  'B1855+09_red_noise_gamma': 3}
+            psc = utils.get_coefficients(pta, ps)
 
-        d1 = ptac.get_delay(psc)[0]
-        d2 = (np.dot(pta.pulsarmodels[0].signals[1].get_basis(ps),
-                     psc['B1855+09_linear_timing_model_coefficients']) +
-              np.dot(pta.pulsarmodels[0].signals[2].get_basis(ps),
-                     psc['B1855+09_basis_ecorr_coefficients']) +
-              np.dot(pta.pulsarmodels[0].signals[3].get_basis(ps),
-                     psc['B1855+09_red_noise_coefficients']))
+            d1 = ptac.get_delay(psc)[0]
+            d2 = (np.dot(pta.pulsarmodels[0].signals[1].get_basis(ps),
+                         psc['B1855+09_linear_timing_model_coefficients']) +
+                  np.dot(pta.pulsarmodels[0].signals[2].get_basis(ps),
+                         psc['B1855+09_basis_ecorr_coefficients']) +
+                  np.dot(pta.pulsarmodels[0].signals[3].get_basis(ps),
+                         psc['B1855+09_red_noise_coefficients']))
 
-        msg = 'Implicit and explicit PTA delays are different.'
-        assert np.allclose(d1, d2), msg
+            msg = 'Implicit and explicit PTA delays are different.'
+            assert np.allclose(d1, d2), msg
 
-        l1 = pta.get_lnlikelihood(ps)
-        l2 = ptac.get_lnlikelihood(psc)
+            l1 = pta.get_lnlikelihood(ps)
+            l2 = ptac.get_lnlikelihood(psc)
 
-        # I don't know how to integrate l2 to match l1...
-        msg = 'Marginal and hierarchical likelihoods should be different.'
-        assert l1 != l2, msg
+            # I don't know how to integrate l2 to match l1...
+            msg = 'Marginal and hierarchical likelihoods should be different.'
+            assert l1 != l2, msg
 
 
 class TestGPCoefficientsPint(TestGPCoefficients):
