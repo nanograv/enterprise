087cce1ba6a368a69040878f9192293761331802
Michele Vallisneri <Michele.Vallisneri@jpl.nasa.gov>
2021-08-30 11:26:02 -0700

    Black

--- enterprise/signals/gp_signals.py
+++ enterprise/signals/gp_signals.py
@@ -777,6 +777,7 @@ def WidebandTimingModel(
 # experimental versions of FourierBasisCommonGP and BasisCommonGP2 that support selections
 # note that Tspan must be provided to FourierBasisCommonGP2
 
+
 def FourierBasisCommonGP2(
     spectrum,
     orf,
