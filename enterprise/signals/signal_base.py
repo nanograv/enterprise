61196d40714aa3d057a116377297bbfa8244b898
Michele Vallisneri <vallis@vallis.org>
2022-05-27 17:00:30 -0700

    Fail better

--- enterprise/signals/signal_base.py
+++ enterprise/signals/signal_base.py
@@ -1088,7 +1088,11 @@ class ndarray_alt(np.ndarray):
     """Sub-class of ``np.ndarray`` with custom ``solve`` method."""
 
     def __new__(cls, inputarr):
+        if inputarr.ndim != 1:
+            raise NotImplementedError("ndarray_alt does not support non-diagonal arrays") 
+
         obj = np.asarray(inputarr).view(cls)
+
         return obj
 
     def __add__(self, other):
@@ -1285,7 +1289,6 @@ class ShermanMorrison(object):
         return logdet
 
     def solve(self, other, left_array=None, logdet=False):
-
         if other.ndim == 1:
             if left_array is None:
                 ret = self._solve_D1(other)
@@ -1297,7 +1300,7 @@ class ShermanMorrison(object):
                 raise TypeError
         elif other.ndim == 2:
             if left_array is None:
-                raise TypeError
+                raise NotImplementedError("ShermanMorrison does not implement _solve_D2")
             elif left_array is not None and left_array.ndim == 2:
                 ret = self._solve_2D2(other, left_array)
             elif left_array is not None and left_array.ndim == 1:
