5854c0dafbe745ae99616bb18a8e7f66a998870c
Anne Archibald <Anne.Archibald@nanograv.org>
2023-02-14 16:51:51 +0000

    Framework for storing things in HDF5 files

---
+++ enterprise/h5format.py
@@ -0,0 +1,132 @@
+from dataclasses import dataclass
+from io import StringIO
+from pathlib import Path
+from textwrap import indent, dedent
+from typing import Optional, Callable, List, IO, Union
+
+import h5py
+import numpy as np
+
+
+class MissingAttribute(ValueError):
+    pass
+
+
+@dataclass
+class H5Entry:
+    name: str
+    description: str
+    required: bool = True
+    use_dataset: bool = False
+    attribute: Optional[str] = None
+    write: Optional[Callable] = None
+    read: Optional[Callable] = None
+
+    def write_to_hdf5(self, h5file: h5py.File, thing):
+        attribute = self.name if self.attribute is None else self.attribute
+        if self.write is not None:
+            return self.write(h5file, thing, attribute)
+        if not hasattr(thing, attribute):
+            if self.required:
+                raise MissingAttribute(f"Attribute {attribute} needed for HDF5 {self.name} missing from {thing}")
+            else:
+                return
+        value = getattr(thing, attribute)
+        if self.use_dataset:
+            if isinstance(value, dict):
+                write_dict_to_hdf5(h5file, self.name, value)
+            else:
+                h5file.create_dataset(
+                    self.name,
+                    data=value,
+                    compression="gzip",
+                    compression_opts=9,
+                )
+        else:
+            try:
+                h5file.attrs[self.name] = value
+            except TypeError as e:
+                raise TypeError(f"Invalid type for storage in an attribute: {type(getattr(thing,attribute))}") from e
+
+    def read_from_hdf5(self, h5file: h5py.File, thing):
+        attribute = self.name if self.attribute is None else self.attribute
+        if self.read is not None:
+            return self.read(h5file, thing, attribute)
+        try:
+            if self.use_dataset:
+                value = h5file[self.name]
+                if isinstance(value, h5py.Group):
+                    value = read_dict_from_hdf5(value)
+                else:
+                    value = np.array(value)
+            else:
+                value = h5file.attrs[self.name]
+        except KeyError:
+            if self.required:
+                raise
+            return
+        else:
+            setattr(thing, attribute, value)
+
+    def write_description(self, f: IO[str]):
+        tags = ["dataset" if self.use_dataset else "attribute"]
+        # tag for type
+        # tag for attribute/dataset
+        # tag for required/optional
+        print(f"* `{self.name}` {', '.join(tags)}", file=f)
+        f.write(indent(dedent(self.description), 4 * " "))
+
+
+def write_dict_to_hdf5(h5group: h5py.Group, name: str, d: dict):
+    g = h5group.create_group(name)
+    for k, v in d.items():
+        if isinstance(v, dict):
+            write_dict_to_hdf5(g, k, v)
+        elif isinstance(v, np.ndarray):
+            g.create_dataset(k, data=v, compression="gzip", compression_level=9)
+        else:
+            g.attrs[k] = v
+
+
+def read_dict_from_hdf5(h5group: h5py.Group) -> dict:
+    r = dict(h5group.attrs)
+    for k, v in h5group.items():
+        if isinstance(v, h5py.Group):
+            r[k] = read_dict_from_hdf5(v)
+        else:
+            r[k] = np.array(v)
+    return r
+
+
+class H5Format:
+    def __init__(self, description_intro: str, entries: Optional[List[H5Entry]] = None, description_finale: str = ""):
+        self.description_intro = dedent(description_intro)
+        self.description_finale = dedent(description_finale)
+        self.entries = [] if entries is None else entries
+
+    def add_entry(self, entry: H5Entry):
+        self.entries.append(entry)
+
+    def write_description(self, f: IO[str]):
+        f.write(self.description_intro)
+        for entry in self.entries:
+            entry.write_description(f)
+        f.write("\n")
+        f.write(self.description_finale)
+
+    def save_to_hdf5(self, h5path: Union[Path, str], thing):
+        with h5py.File(h5path, "w") as f:
+            f.attrs["description"] = self.description
+            for entry in self.entries:
+                entry.write_to_hdf5(f, thing)
+
+    def load_from_hdf5(self, h5path: Union[Path, str], thing):
+        with h5py.File(h5path, "r") as f:
+            for entry in self.entries:
+                entry.read_from_hdf5(f, thing)
+
+    @property
+    def description(self) -> str:
+        f = StringIO()
+        self.write_description(f)
+        return f.getvalue()
---
+++ tests/test_h5format.py
@@ -0,0 +1,253 @@
+from io import StringIO
+from pathlib import Path
+
+import pytest
+import numpy as np
+
+from enterprise.h5format import H5Format, H5Entry
+
+
+@pytest.fixture
+def simple_format() -> H5Format:
+    return H5Format(
+        description_intro="""\
+            # Simple testing format
+
+            This is an HDF5 "format" designed to exercise the basic tools.
+            """,
+        description_finale="""\
+            ## Final notes
+
+            You can add some ending comments here.
+            """,
+    )
+
+
+def test_description_intro(simple_format: H5Format):
+    assert simple_format.description.startswith("# Simple testing format")
+
+
+def test_description_finale(simple_format: H5Format):
+    assert "\n## Final notes" in simple_format.description
+
+
+def test_description_includes_entry(simple_format: H5Format):
+    simple_format.add_entry(H5Entry(name="an_entry", description="This is a sample entry."))
+    assert "an_entry" in simple_format.description
+
+
+def test_string_entry_description():
+    e = H5Entry(
+        name="the_name",
+        description="""\
+        Quick testing entry.
+        """,
+    )
+    f = StringIO()
+    e.write_description(f)
+    d = f.getvalue()
+    print(d)
+    assert d.startswith("* `the_name` ")
+    assert "attribute" in d
+    assert "\n    Quick" in d
+    assert d.endswith("\n")
+
+
+def test_string_entry_description_dataset():
+    e = H5Entry(
+        name="the_name",
+        description="""\
+        Quick testing entry.
+        """,
+        use_dataset=True,
+    )
+    f = StringIO()
+    e.write_description(f)
+    d = f.getvalue()
+    print(d)
+    assert "dataset" in d
+
+
+def test_write_succeeds(tmp_path: Path, simple_format: H5Format):
+    h5path = tmp_path / "test.hdf5"
+
+    simple_format.add_entry(
+        H5Entry(
+            name="an_entry",
+            description="This is a sample entry.",
+        )
+    )
+
+    class Thing:
+        pass
+
+    thing = Thing()
+    thing.an_entry = "fish"
+    simple_format.save_to_hdf5(h5path, thing)
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "fish",
+        7,
+        7.0,
+        np.float16(2.3),
+        np.float32(2.5),
+        np.float64(1.7),
+    ],
+)
+def test_write_read_scalar(tmp_path, simple_format, value):
+    h5path = tmp_path / "test.hdf5"
+    simple_format.add_entry(
+        H5Entry(
+            name="an_entry",
+            description="This is a sample entry.",
+        )
+    )
+
+    class Thing:
+        pass
+
+    thing = Thing()
+    thing.an_entry = value
+    simple_format.save_to_hdf5(h5path, thing)
+
+    recovered_thing = Thing()
+    simple_format.load_from_hdf5(h5path, recovered_thing)
+
+    assert recovered_thing.an_entry == thing.an_entry
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        [1, 2, 3],
+        ["cod", "plaice"],
+        [1.7, 2.3, 0.1],
+        np.zeros((2, 3, 4)),
+    ],
+)
+def test_write_read_vector(tmp_path, simple_format, value):
+    h5path = tmp_path / "test.hdf5"
+    simple_format.add_entry(
+        H5Entry(
+            name="an_entry",
+            description="This is a sample entry.",
+        )
+    )
+
+    class Thing:
+        pass
+
+    thing = Thing()
+    thing.an_entry = value
+    simple_format.save_to_hdf5(h5path, thing)
+
+    recovered_thing = Thing()
+    simple_format.load_from_hdf5(h5path, recovered_thing)
+
+    assert np.array_equal(recovered_thing.an_entry, thing.an_entry)
+    assert isinstance(recovered_thing.an_entry, np.ndarray)
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        {1: 2, 3: 4},
+        dict(fish=1, fowl=2),
+        dict(fish="cod", fowl="pheasant"),
+    ],
+)
+def test_write_read_dict_raises(tmp_path, simple_format, value):
+    h5path = tmp_path / "test.hdf5"
+    simple_format.add_entry(
+        H5Entry(
+            name="an_entry",
+            description="This is a sample entry.",
+        )
+    )
+
+    class Thing:
+        pass
+
+    thing = Thing()
+    thing.an_entry = value
+    with pytest.raises(TypeError) as e:
+        simple_format.save_to_hdf5(h5path, thing)
+    assert "dict" in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        [1, 2, 3],
+        [1.7, 2.3, 0.1],
+        np.zeros((2, 3, 4)),
+    ],
+)
+def test_write_read_vector_dataset(tmp_path, simple_format, value):
+    h5path = tmp_path / "test.hdf5"
+    simple_format.add_entry(
+        H5Entry(
+            name="an_entry",
+            description="This is a sample entry.",
+            use_dataset=True,
+        )
+    )
+
+    class Thing:
+        pass
+
+    thing = Thing()
+    thing.an_entry = value
+    simple_format.save_to_hdf5(h5path, thing)
+
+    recovered_thing = Thing()
+    simple_format.load_from_hdf5(h5path, recovered_thing)
+
+    assert np.array_equal(recovered_thing.an_entry, thing.an_entry)
+    assert isinstance(recovered_thing.an_entry, np.ndarray)
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        dict(fish=1, fowl=2),
+        dict(fish="cod", fowl="pheasant"),
+        dict(fish=[1, 2, 3], fowl=dict(hare=1, hounds="dogs")),
+    ],
+)
+def test_write_read_dict_dataset(tmp_path, simple_format, value):
+    h5path = tmp_path / "test.hdf5"
+    simple_format.add_entry(
+        H5Entry(
+            name="an_entry",
+            description="This is a sample entry.",
+            use_dataset=True,
+        )
+    )
+
+    class Thing:
+        pass
+
+    thing = Thing()
+    thing.an_entry = value
+    simple_format.save_to_hdf5(h5path, thing)
+
+    recovered_thing = Thing()
+    simple_format.load_from_hdf5(h5path, recovered_thing)
+
+    def compare_dicts(d1, d2):
+        assert set(d1.keys()) == set(d2.keys())
+        for k, v1 in d1.items():
+            v2 = d2[k]
+            if isinstance(v1, dict):
+                assert isinstance(v2, dict)
+                compare_dicts(v1, v2)
+            elif isinstance(v1, np.ndarray):
+                assert np.array_equal(v1, v2)
+            else:
+                assert v1 == v2
+
+    compare_dicts(recovered_thing.an_entry, thing.an_entry)
