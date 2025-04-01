34ddc1c52396b22e5e612db7ce712418f8cf52a4
Rutger van Haasteren <vhaasteren@gmail.com>
2025-04-01 11:47:18 +0200

    I forgot to lint my added line, yet again

--- enterprise/signals/utils.py
+++ enterprise/signals/utils.py
@@ -327,7 +327,7 @@ def create_spindown_timing_model(toas, order=2):
     """
 
     avetoas = (toas - np.mean(toas)) / np.mean(toas)
-    designmatrix = avetoas[:,None]**np.arange(order+1)[None,:]
+    designmatrix = avetoas[:, None] ** np.arange(order + 1)[None, :]
     parameter_names = ["Offset"] + ["F{ii}".format(ii=ii) for ii in range(order)]
 
     return designmatrix, parameter_names
